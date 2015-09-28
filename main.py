00e31b163ffd8f53c7fed40114a25f74d9145fb3
Ayrton Vercruysse <ayrton.vercruysse@gmail.com>
2015-09-28 21:30:28 +0200

    added led endpoints

--- main.py
+++ main.py
@@ -19,6 +19,38 @@ def LEDoff(zone):
  os.system(string) 
  return "Test succeeded"
 
+@app.rout("/milight/white/<zone>", methods=['PUT'])
+def LEDwhite(zone):
+ string = "~/milight_binaries/milight %s white" %  zone
+ os.system(string) 
+ return "Test succeeded"
+
+@app.rout("/milight/disco/<zone>", methods=['PUT'])
+def LEDwhite(zone):
+ string = "~/milight_binaries/milight %s disco" %  zone
+ os.system(string) 
+ return "Test succeeded"
+
+ @app.rout("/milight/disco/<zone>/<speed>", methods=['PUT'])
+def LEDwhite(zone, speed):
+ string = "~/milight_binaries/milight %s disco %s" %  (zone, speed)
+ os.system(string) 
+ return "Test succeeded"
+
+@app.rout("/milight/brightness/<zone>/<value>", methods=['PUT'])
+def LEDwhite(zone, value):
+ string = "~/milight_binaries/milight %s brightness %s" %  (zone, value)
+ os.system(string) 
+ return "Test succeeded"
+
+@app.rout("/milight/color/<zone>/<value>", methods=['PUT'])
+def LEDwhite(zone, value):
+ string = "~/milight_binaries/milight %s color %s" %  (zone, value)
+ os.system(string) 
+ return "Test succeeded"
+
+
+
 if __name__ == "__main__":
 
  app.run(host='0.0.0.0');
